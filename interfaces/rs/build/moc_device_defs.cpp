32a28f849ae33bac13fe4a2d8eca0cff1289097a
breton75 <breton75@yandex.ru>
2023-04-21 17:49:20 +0300

    Update Makefile, moc_device_defs.cpp, and 9 more files...

--- interfaces/rs/build/moc_device_defs.cpp
+++ interfaces/rs/build/moc_device_defs.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_modus__IOBuffer_t {
-    QByteArrayData data[6];
-    char stringdata0[59];
+    QByteArrayData data[9];
+    char stringdata0[73];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,11 +34,14 @@ QT_MOC_LITERAL(1, 16, 10), // "readyWrite"
 QT_MOC_LITERAL(2, 27, 0), // ""
 QT_MOC_LITERAL(3, 28, 12), // "modus::BUFF*"
 QT_MOC_LITERAL(4, 41, 6), // "buffer"
-QT_MOC_LITERAL(5, 48, 10) // "dataReaded"
+QT_MOC_LITERAL(5, 48, 10), // "dataReaded"
+QT_MOC_LITERAL(6, 59, 6), // "notice"
+QT_MOC_LITERAL(7, 66, 3), // "say"
+QT_MOC_LITERAL(8, 70, 2) // "ba"
 
     },
     "modus::IOBuffer\0readyWrite\0\0modus::BUFF*\0"
-    "buffer\0dataReaded"
+    "buffer\0dataReaded\0notice\0say\0ba"
 };
 #undef QT_MOC_LITERAL
 
@@ -48,20 +51,24 @@ static const uint qt_meta_data_modus__IOBuffer[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       2,       // signalCount
+       4,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   24,    2, 0x06 /* Public */,
-       5,    1,   27,    2, 0x06 /* Public */,
+       1,    1,   34,    2, 0x06 /* Public */,
+       5,    1,   37,    2, 0x06 /* Public */,
+       6,    1,   40,    2, 0x06 /* Public */,
+       7,    1,   43,    2, 0x06 /* Public */,
 
  // signals: parameters
     QMetaType::Void, 0x80000000 | 3,    4,
     QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, QMetaType::QByteArray,    8,
 
        0        // eod
 };
@@ -74,6 +81,8 @@ void modus::IOBuffer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int 
         switch (_id) {
         case 0: _t->readyWrite((*reinterpret_cast< modus::BUFF*(*)>(_a[1]))); break;
         case 1: _t->dataReaded((*reinterpret_cast< modus::BUFF*(*)>(_a[1]))); break;
+        case 2: _t->notice((*reinterpret_cast< modus::BUFF*(*)>(_a[1]))); break;
+        case 3: _t->say((*reinterpret_cast< const QByteArray(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -89,6 +98,18 @@ void modus::IOBuffer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int 
             typedef void (IOBuffer::*_t)(modus::BUFF * );
             if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&IOBuffer::dataReaded)) {
                 *result = 1;
+            }
+        }
+        {
+            typedef void (IOBuffer::*_t)(modus::BUFF * );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&IOBuffer::notice)) {
+                *result = 2;
+            }
+        }
+        {
+            typedef void (IOBuffer::*_t)(const QByteArray & );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&IOBuffer::say)) {
+                *result = 3;
             }
         }
     }
@@ -119,13 +140,13 @@ int modus::IOBuffer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 4;
     }
     return _id;
 }
@@ -142,5 +163,19 @@ void modus::IOBuffer::dataReaded(modus::BUFF * _t1)
 {
     void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
     QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void modus::IOBuffer::notice(modus::BUFF * _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 2, _a);
+}
+
+// SIGNAL 3
+void modus::IOBuffer::say(const QByteArray & _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 3, _a);
 }
 QT_END_MOC_NAMESPACE
